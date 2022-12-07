bc01fa2f9a86993bb2f515c9a94c0561f0bd99c5
HA S AN <117574358+hasanraj720@users.noreply.github.com>
2022-12-07 14:51:18 +0600

    h1s19-v2
    pkg install git
    git clone https://github.com/hasanraj720/h1s19.git
    cd h1s19-v2
    python2 h1s19-v2.py

---
+++ HA S AN-V2.py
@@ -0,0 +1,142 @@
+#!/usr/bin/python
+
+"""
+Author             : HA S AN
+contact            : sumaiyaaktersathy47@gmail.com
+Github             : https://github.com/hasanraj720
+My Youtube Channel : https://youtube.com/@The_All_Tube_BD
+
+Subcribe My Youtube Channel To Learn Ethical Hacking ^_^
+"""
+
+import sys
+import os.path
+import subprocess
+
+ntfile = ['.module', 'lib']
+
+ubuntu = '' + str(open('.module/jalurU.mh').read())
+termux = '' + str(open('.module/jalurT.mh').read())
+termlb = '' + str(open('.module/ngentot.mh').read())
+instal = '' + str(open('.module/IN.mh').read())
+
+
+def _main_():
+    if os.path.isdir('' + ubuntu.strip()):
+        if os.getuid() != 0:
+            print '[x] Failed: your must be root'
+            sys.exit()
+
+        if not os.path.isdir(ntfile[0]):
+            print '[x] Failed: no directory module'
+            sys.exit()
+
+        if not os.path.isdir(ntfile[1]):
+            print '[x] Failed: no directory lib'
+            sys.exit()
+        else:
+
+            print '' + instal.strip()
+
+            # ==================================================================#
+
+            os.system('python .module/files/' + str(open('.module/A.mh'
+                      ).read()))
+            os.system('python .module/files/' + str(open('.module/B.mh'
+                      ).read()))
+            os.system('python .module/files/' + str(open('.module/C.mh'
+                      ).read()))
+            os.system('python .module/files/' + str(open('.module/D.mh'
+                      ).read()))
+            os.system('python .module/files/' + str(open('.module/E.mh'
+                      ).read()))
+            os.system('python .module/files/' + str(open('.module/F.mh'
+                      ).read()))
+            os.system('python .module/files/' + str(open('.module/G.mh'
+                      ).read()))
+            os.system('python .module/files/' + str(open('.module/H.mh'
+                      ).read()))
+
+            # ==================================================================#
+
+            if os.path.isdir('/usr/bin/lib'):
+                os.system('rm -rf /usr/bin/lib')
+                os.system('' + str(open('.module/pindahU.mh').read()))
+                os.system('' + str(open('.module/PindahU.mh').read()))
+
+            if not os.path.isdir('/usr/bin/lib'):
+                os.system('' + str(open('.module/pindahU.mh').read()))
+                os.system('' + str(open('.module/PindahU.mh').read()))
+
+            # ==================================================================#
+
+            print '' + str(open('.module/DU.la').read())
+            print '' + str(open('.module/Du').read())
+            os.system('python .JM.xn')
+
+            # ==================================================================#
+
+    if os.path.isdir('' + termux.strip()):
+        if not os.path.isdir('' + termux.strip()):
+            sys.exit()
+
+        if not os.path.isdir(ntfile[0]):
+            print '[x] Failed: no directory module'
+            sys.exit()
+
+        if not os.path.isdir(ntfile[1]):
+            print '[x] Failed: no directory lib'
+            sys.exit()
+        else:
+
+            print '' + instal.strip()
+  __    __       ___           _______.     ___      .__   __. 
+ |  |  |  |     /   \         /       |    /   \     |  \ |  | 
+ |  |__|  |    /  ^  \       |   (----`   /  ^  \    |   \|  | 
+ |   __   |   /  /_\  \       \   \      /  /_\  \   |  . `  | 
+ |  |  |  |  /  _____  \  .----)   |    /  _____  \  |  |\   | 
+ |__|  |__| /__/     \__\ |_______/    /__/     \__\ |__| \__| 
+                                                               
+
+
+            # ==============================================================#
+
+            os.system('python2 .module/' + str(open('.module/A.mh'
+                      ).read()))
+            os.system('python2 .module/' + str(open('.module/B.mh'
+                      ).read()))
+            os.system('python2 .module/' + str(open('.module/C.mh'
+                      ).read()))
+            os.system('python2 .module/' + str(open('.module/D.mh'
+                      ).read()))
+            os.system('python2 .module/' + str(open('.module/E.mh'
+                      ).read()))
+            os.system('python2 .module/' + str(open('.module/F.mh'
+                      ).read()))
+            os.system('python2 .module/' + str(open('.module/G.mh'
+                      ).read()))
+            os.system('python2 .module/' + str(open('.module/H.mh'
+                      ).read()))
+
+            # ==============================================================#
+
+            if os.path.isdir('' + termlb.strip()):
+                os.system('' + str(open('.module/pacar.mh').read()))
+                os.system('' + str(open('.module/HA S AN.mh').read()))
+                os.system('' + str(open('.module/HA S AN.txt').read()))
+
+            if not os.path.isdir('' + termlb.strip()):
+                os.system('' + str(open('.module/HA S AN.mh').read()))
+                os.system('' + str(open('.module/HA S AN.txt').read()))
+
+            # ==============================================================#
+
+            print '' + str(open('.module/DU.la').read())
+            print '' + str(open('.module/Du').read())
+            os.system('python2 .JM.xn && cd')
+
+
+            # ==============================================================#
+
+if __HA S AN__ == '__main__':
+    _main_()
